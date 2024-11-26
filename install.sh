2bcbaa9166098002bff1658113c5c77ccfe29a58
fgcczsdfjvf <tomsliu857@outlook.com>
2024-11-25 20:41:04 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+cd /var/www/html
 wget http://www.winmail.cn/download/WM2012-WebMail-Linux.tar.gz
 tar -zxvf WM2012-WebMail-Linux.tar.gz
 rm -f WM2012-WebMail-Linux.tar.gz
-chown -R www-data:www-data /var/www/html
