c7d710f3a6679b8cc3f68fd6165f36343b56b857
fgcczsdfjvf <tomsliu857@outlook.com>
2024-11-25 20:20:56 -0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 设置环境变量
+export PORT=${PORT:-3000}
+
+# 启动Winmail服务
+exec docker run \
+    -p ${PORT}:80 \
+    -e TZ=Asia/Shanghai \
+    --name winmail \
+    lixg/winmail:latest
