e2608a863918889d1cba8ddee463608bea5879d5
fgcczsdfjvf <tomsliu857@outlook.com>
2024-11-25 20:36:23 -0800

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# 确保Apache监听正确的端口
-sed -i "s/80/8080/g" /etc/apache2/ports.conf
-sed -i "s/80/8080/g" /etc/apache2/sites-enabled/000-default.conf
-
-# 启动Apache
-apache2ctl -D FOREGROUND
