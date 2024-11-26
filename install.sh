4e620b4d5faee51fc533c562383410041bf3f9a6
fgcczsdfjvf <tomsliu857@outlook.com>
2024-11-25 20:39:10 -0800

    Update and rename nstall.sh to install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-
-# 下载 Winmail
 wget http://www.winmail.cn/download/WM2012-WebMail-Linux.tar.gz
 tar -zxvf WM2012-WebMail-Linux.tar.gz
 rm -f WM2012-WebMail-Linux.tar.gz
-
-# 设置权限
-chmod -R 755 .
+chown -R www-data:www-data /var/www/html
