9559ea16c7ee391403ecbc11ba8876fae1135404
fgcczsdfjvf <tomsliu857@outlook.com>
2024-11-25 20:29:47 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# 设置环境变量
-export PORT=${PORT:-3000}
+# 启动 Apache
+apache2ctl start
 
-# 启动Winmail服务
-exec docker run \
-    -p ${PORT}:80 \
-    -e TZ=Asia/Shanghai \
-    --name winmail \
-    ${DOCKER_USERNAME}/winmail-docker:latest
+# 保持容器运行
+tail -f /dev/null
